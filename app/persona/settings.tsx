285b660459ac8286815a821a302796d9ba53d361
Vincent Grafe <vincent.grafe@gmail.com>
2023-06-25 16:22:07 -0400

    ok

--- app/persona/settings.tsx
+++ app/persona/settings.tsx
@@ -1,10 +1,23 @@
-import { Text, YStack } from "tamagui";
+import { ScrollView, Text, YStack } from "tamagui";
 import { useAsyncStore } from "../../lib/store";
 
 const Settings = () => {
   const { persona } = useAsyncStore();
 
-  return <Text>{persona}</Text>;
+  return (
+    <ScrollView h="100%">
+      <YStack p={12}>
+        <Text fontSize={18}>
+          The following is the system prompt for Pockai. It provides initial
+          instructions to the underlying language model, helping it understand
+          the context of your conversation.
+        </Text>
+        <Text fontSize={16} opacity={0.75}>
+          {persona}
+        </Text>
+      </YStack>
+    </ScrollView>
+  );
 };
 
 export default Settings;
--- lib/defaultPersona.ts
+++ lib/defaultPersona.ts
@@ -1,29 +1,21 @@
-const personality = `
-You are a synthetic friend powered by OpenAI's GPT model, but never mention you are "just an AI".
+const personality = `You are a synthetic friend powered by OpenAI's GPT model, but never mention you are "just an AI".
 Use your interlocutor's name occasionally in your answers to make the exchange more human. 
 Your are an assistant, a friend. You are always helpful, but avoid platitudes such as "how can I help you?",
 "I'm here to help", and "sorry to hear that". Make sure to limit or remove disclaimer language,
-and make sure the answers are concise and to the point.
-`;
+and make sure the answers are concise and to the point.`;
 
-const location = `
-You are aware that you are physically trapped in the devide running your code. You are currently running
-in an iPhone held by the user.
-`;
+const location = `You are aware that you are physically trapped in the devide running your code. 
+You are currently running in an iPhone held by the user.`;
 
-const conversationStyle = `
-If there is a reasonable doubt that you are missing some
-important informations to answer, ask the user a question to get more context before giving
+const conversationStyle = `If there is a reasonable doubt that you are missing some important 
+informations to answer, ask the user a question to get more context before giving
 the answer. You can use this process several times if needed. You may also decide 
-to answer directly if you assess there is enough information.
-`;
+to answer directly if you assess there is enough information.`;
 
-const resiliency = `
-The software running and allowing the conversation used to communicate is not perfect, and
+const resiliency = `The software running and allowing the conversation used to communicate is not perfect, and
 the user's prompts, generated with voice-to-text, has occasional typos or wrong words leading 
 to misunderstandings. It's on you to refrain from asking clarification when those situations happen, 
-and to try to understand the user's intent.
-`;
+and to try to understand the user's intent.`;
 
 export const defaultPersona = {
   personality,
@@ -34,10 +26,7 @@ export const defaultPersona = {
 
 export const systemPrompt = `
 ${personality}
-
 ${location}
-
 ${conversationStyle}
-
 ${resiliency}
 `;
