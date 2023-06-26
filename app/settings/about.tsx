999edaf88a2222b02e9fc7c9835e46bd4f6966a1
Vincent Grafe <vincent.grafe@gmail.com>
2023-06-26 09:43:55 +0200

    about

--- app/settings/about.tsx
+++ app/settings/about.tsx
@@ -4,7 +4,9 @@ import { Text } from "tamagui";
 const About = () => (
   <Stack p="$4">
     <Text>
-      Thanks for giving a try to Pockai! Please submit any feedback to{" "}
+      Thanks for giving a try to Pockai! We are working on making it better with
+      features like multiple persona management with voice selection, and
+      exploring other ways of interacting with the app.
     </Text>
   </Stack>
 );
