4c16099aff429a2ffa10c93e6346ec338bbea750
Vincent Grafe <vincent.grafe@gmail.com>
2023-07-25 22:16:23 -0400

    ok

--- app/(tabs)/_layout.tsx
+++ app/(tabs)/_layout.tsx
@@ -16,7 +16,7 @@ export default function TabLayout() {
       <Tabs.Screen
         name="index"
         options={{
-          title: "Tab One",
+          title: "chat",
           tabBarIcon: ({ color }) => <Smile color={color} />,
           headerShown: false,
           headerRight: () => (
