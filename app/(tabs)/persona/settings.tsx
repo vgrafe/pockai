3f9b76ca0fd82fae3cb1383dae5beb620e2b0a36
Vincent Grafe <vincent.grafe@gmail.com>
2023-07-25 22:21:36 -0400

    ok

--- app/(tabs)/persona/settings.tsx
+++ app/(tabs)/persona/settings.tsx
@@ -1,4 +1,4 @@
-import { ScrollView, View } from "react-native";
+import { StyleSheet, ScrollView, View } from "react-native";
 import { useAsyncStore } from "@/lib/store";
 import { Text } from "@/components/Themed";
 
@@ -11,17 +11,24 @@ const PersonaSettings = () => {
         style={{
           margin: 24,
           marginTop: 96,
+          display: "flex",
+          justifyContent: "center",
+          alignItems: "center",
         }}
       >
-        <Text>
+        <Text style={styles.text}>
           The following is the system prompt for Pockai. It provides initial
           instructions to the underlying language model, helping it understand
           the context of your conversation.
         </Text>
-        <Text>{persona}</Text>
+        <Text style={styles.text}>{persona}</Text>
       </View>
     </ScrollView>
   );
 };
+
+const styles = StyleSheet.create({
+  text: { fontSize: 18, lineHeight: 26 },
+});
 
 export default PersonaSettings;
