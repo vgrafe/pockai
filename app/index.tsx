dba954dda974de4cd5040c3eb53c9c576321cb10
Vincent Grafe <vincent.grafe@gmail.com>
2023-08-02 11:35:59 -0400

    k

--- app/index.tsx
+++ app/index.tsx
@@ -3,37 +3,44 @@ import { Text } from "@/components/Text";
 import { View } from "react-native";
 import { useAsyncStore } from "@/lib/store";
 import { Link } from "expo-router";
+import { styles } from "@/lib/styles";
 
 const Contacts = () => {
   const contacts = useAsyncStore((a) => a.contacts);
 
-  return (
-    <View style={{ gap: 12, margin: 12 }}>
-      {contacts.length ? (
-        <View>
-          {contacts.map((contact) => (
-            <List.Item key={contact.id}>
-              <Link href={`/contacts/${contact.id}`}>
-                <Text>{contact.name}</Text>
-              </Link>
-            </List.Item>
-          ))}
+  if (contacts.length === 0) {
+    return (
+      <>
+        <View style={styles.centeredContent}>
+          <Text>no contacts</Text>
         </View>
-      ) : (
-        <Text>no contacts</Text>
-      )}
-      <View>
-        <List.Item>
-          <Link href="/contacts/jean/chat">
-            <Text>Jean</Text>
+        <View style={{ position: "fixed", bottom: 80 }}>
+          <Text>debug</Text>
+
+          <List.Item>
+            <Link href="/contacts/jean/chat">
+              <Text>Jean</Text>
+            </Link>
+          </List.Item>
+          <List.Item>
+            <Link href="/contacts/cule/chat">
+              <Text>Cule</Text>
+            </Link>
+          </List.Item>
+        </View>
+      </>
+    );
+  }
+
+  return (
+    <View>
+      {contacts.map((contact) => (
+        <List.Item key={contact.id}>
+          <Link href={`/contacts/${contact.id}`}>
+            <Text>{contact.name}</Text>
           </Link>
         </List.Item>
-        <List.Item>
-          <Link href="/contacts/cule/chat">
-            <Text>Cule</Text>
-          </Link>
-        </List.Item>
-      </View>
+      ))}
     </View>
   );
 };
